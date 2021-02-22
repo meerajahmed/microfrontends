b80159a35f2b473d498bfe3cd130f129dcf35291
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2021-02-22 15:15:33 +0530

    delegating shared module selection

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
     mode: 'development',
@@ -16,7 +17,8 @@ const devConfig = {
             name: 'container',
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-            }
+            },
+            shared: packageJson.dependencies
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
     mode: 'development',
@@ -17,7 +18,8 @@ const devConfig = {
             filename: 'remoteEntry.js',
             exposes: {
                 './bootstrap':'./src/bootstrap.js'
-            }
+            },
+            shared: packageJson.dependencies,
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
