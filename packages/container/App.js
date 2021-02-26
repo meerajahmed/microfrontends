be64ef18a56fd60ab1bc34b3a43bf47bef5eca09
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2021-02-26 19:30:05 +0530

    fix css name collisions

--- packages/container/App.js
+++ packages/container/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingRoot from './src/components/MarketingRoot';
+import Header from './src/components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: '_cont'
+});
 
 const App = () => {
     return (
-        <MarketingRoot />
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <Header />
+                <MarketingRoot />
+            </StylesProvider>
+        </BrowserRouter>
     );
 };
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { StylesProvider } from "@material-ui/core/styles"
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: '_mark'
+});
+
 
 const App = () => {
     return (
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing} />
